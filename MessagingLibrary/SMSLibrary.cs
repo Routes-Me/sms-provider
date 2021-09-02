b7006c60d0aff8c4bd39f3714923cece66147638
vivian tharakan <vtharaka@routesme.com>
2021-09-02 15:06:01 +0300

    Final tested version for lib

---
+++ MessagingLibrary/SMSLibrary.cs
@@ -0,0 +1,50 @@
+﻿using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace MessagingLibraries
+{
+    public static class MessagingLibrary
+    {
+
+        public static async Task<string> SendSMS(string phoneNumber, string message, int languageCode)
+        {
+            string domainURL = "https://www.kwtsms.com/API/send/?username=routes&password=routes_@!3015";
+            var requestURL = $"{domainURL}&sender=Routes&mobile={phoneNumber}&lang={languageCode}&message={message}";
+
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync(requestURL))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    return apiResponse;
+                }
+            }
+        }
+
+        public static async Task<string> CheckMessageStatus(string messageId)
+        {//https://www.kwtsms.com/API/dlr/?username=myuser&password=mypass&msgid=xxxxxxxxx
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync($"https://www.kwtsms.com/API/dlr/?username=routes&password=routes_@!3015&msgid={messageId}"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    return apiResponse;
+                }
+            }
+        }
+
+        public static async Task<string> CheckBalance()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("https://www.kwtsms.com/API/balance/?username=routes&password=routes_@!3015"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    return apiResponse;
+                }
+            }
+        }
+
+
+    }
+}
